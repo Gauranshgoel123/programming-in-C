cf337c9670b99a87b4fbc49ca698cd49afba1940
Gauranshgoel123 <121503835+Gauranshgoel123@users.noreply.github.com>
2022-12-27 23:02:06 +0530

    practical uploaded

---
+++ printing even no from 1 to 100.c
@@ -0,0 +1,11 @@
+// C Program To Print Even Numbers From 1 To 100
+#include <stdio.h>
+void main()
+{
+printf("GAURANSH GOEL\nCSE-A\nPRINT EVEN NUMBERS FROM 1 TO 100\n");
+for (int i = 2; i <= 100; i++)
+    {
+    if (i % 2 == 0)
+        printf("%d ", i);
+    }
+}
